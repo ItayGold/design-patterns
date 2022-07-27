e1850c668b639f005e3fe47b16b9a81f96b99d7b
ItayGold <itil.gold@gmail.com>
2022-07-28 00:27:23 +0300

    drink is delicious

--- src/main/java/abstractFactory/Demo.java
+++ src/main/java/abstractFactory/Demo.java
@@ -4,6 +4,8 @@ package abstractFactory;
 import javafx.util.Pair;
 import org.reflections.Reflections;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -53,9 +55,51 @@ class CoffeeFactory implements HotDrinkFactory {
 }
 
 class HotDrinkMachine {
-    private List<Pair<String, HotDrinkFactory>> namedFactories = new ArrayList<>();
+    private final List<Pair<String, HotDrinkFactory>> namedFactories = new ArrayList<>();
 
-    public HotDrinkMachine() {
-        Set<Class<? extends HotDrinkFactory>> subTypesOf = new Reflections("").getSubTypesOf(HotDrinkFactory.class);
+    public HotDrinkMachine() throws Exception {
+        Set<Class<? extends HotDrinkFactory>> types = new Reflections(HotDrinkFactory.class).getSubTypesOf(HotDrinkFactory.class);
+
+        for (Class<? extends HotDrinkFactory> type : types) {
+            namedFactories.add(new Pair<>(
+                    type.getSimpleName().replace("Factory", ""),
+                    type.getDeclaredConstructor().newInstance()
+            ));
+        }
+    }
+
+    public HotDrink makeDrink() throws Exception {
+        System.out.println("Available drinks: ");
+        for (int index = 0; index < namedFactories.size(); ++index) {
+            Pair<String, HotDrinkFactory> item = namedFactories.get(index);
+            System.out.println("" + index + ": " + item.getKey());
+        }
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String s;
+            int i, amount;
+
+            if ((s = reader.readLine()) != null
+                    && (i = Integer.parseInt(s)) >= 0
+                    && i < namedFactories.size()) {
+
+                System.out.println("Specify amount: ");
+                s = reader.readLine();
+
+                if (s != null && (amount = Integer.parseInt(s)) > 0) {
+                    return namedFactories.get(i).getValue().prepare(amount);
+                }
+            }
+            System.out.println("Incorrect input, try again.");
+        }
+    }
+}
+
+class Demo {
+    public static void main(String[] args)  throws Exception{
+        HotDrinkMachine machine = new HotDrinkMachine();
+        HotDrink drink = machine.makeDrink();
+        drink.consume();
     }
 }
